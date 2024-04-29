fbe6fb02a2755bb9bbc282fa1ac0ad01dee36ba4
rohan7408 <rohanpoudel88@gmail.com>
2024-04-29 13:01:52 +0545

    added

---
+++ script.js
@@ -0,0 +1,4 @@
+// Toggle active class on hamburger menu click
+document.querySelector(".toggle").addEventListener("click", function () {
+  document.querySelector(".nav").classList.toggle("active");
+});
